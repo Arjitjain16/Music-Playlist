2d106f076d61ccbe6647f08040eaf623b7185f1b
Your Name <you@example.com>
2024-07-22 12:24:32 -0700

    passing props from parent to child

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,51 @@
+import { useState } from "react"
 import Card from "./assets/components/Card"
 import Navbar from "./assets/components/Navbar"
 
 const App = () => {
+
+  const data = [
+    {
+      "pic": "https://images.unsplash.com/photo-1530419248307-be80b9468e77?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c29uZyUyMHBsYXlsaXN0fGVufDB8fDB8fHww",
+      "name": "One love",
+      "artist": "The hill",
+      "adder": false
+    },
+    {
+      "pic": "https://images.unsplash.com/photo-1474692295473-66ba4d54e0d3?q=80&w=1384&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "name": "Together",
+      "artist": "Seton",
+      "adder": false
+    },
+    {
+      "pic": "https://images.unsplash.com/photo-1518911710364-17ec553bde5d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHNvbmclMjBwbGF5bGlzdHxlbnwwfHwwfHx8MA%3D%3D",
+      "name": "Be Yourself",
+      "artist": "Vans The band",
+      "adder": false
+    },
+    {
+      "pic": "https://images.unsplash.com/photo-1518887572120-cae9ec0daab6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHNvbmclMjBwbGF5bGlzdHxlbnwwfHwwfHx8MA%3D%3D",
+      "name": "Just Up",
+      "artist": "Jevsss",
+      "adder": false
+    },
+    {
+      "pic": "https://images.unsplash.com/photo-1693835777292-cf103dcd2324?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHNvbmclMjBwbGF5bGlzdHxlbnwwfHwwfHx8MA%3D%3D",
+      "name": "Take up",
+      "artist": "Runmmor",
+      "adder": false
+    }
+  ]
+   
+  const [songData, setsongData] = useState(data)
+
   return (
     <div className="">
       <Navbar/>
       <div className="flex items-center flex-row flex-wrap">
-      <Card /> 
+        {songData.map((obj) => (
+          <Card key={obj.name} data={obj} />
+        ))}
       </div>
     </div>
   )
--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -1,15 +1,19 @@
 
-const Card = () => {
+const Card = ({data}) => {
+
+    const {pic , name , artist , adder} = data
   return (
     <div className="px-10 mt-10 ">
         <div className="bg-zinc-200 w-64 h-32 flex items-center py-2 px-3 gap-6 relative rounded-xl">
-            <div className="bg-orange-400 h-8 w-16  px-2 py-10 rounded-md"></div>
+            <div className="bg-orange-400 h-8 w-16 px-2 py-10 rounded-md">
+                <img className="h-full w-full object-cover" src={pic} alt="Hii" />
+            </div>
             <div>
-                <h1 className="text-2xl">Song Name</h1>
-                <p>Artish Name</p>
+                <h1 className="text-2xl">{name}</h1>
+                <p>{artist}</p>
             </div>
             <button className="bg-orange-400 text-xl mt-4 text-zinc-100 py-1 px-5 absolute bottom-0 left-[25%] -translate-y-[-50%] -translate-x-[-50%] rounded-md">
-            Add Fav
+            {adder}
             </button>
         </div>
     </div>
